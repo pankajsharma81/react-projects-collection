747c459df4efd085f83b53143ea9008f6c1b6aea
pankajsharma81 <pankajsharma88279@gmail.com>
2025-02-28 16:37:10 +0530

    added Hero Section

--- brand-page/src/App.jsx
+++ brand-page/src/App.jsx
@@ -1,9 +1,11 @@
 import "./App.css";
+import Main from "./components/HeroSection";
 import Navigation from "./components/Navigation";
 const App = () => {
   return (
     <>
       <Navigation />
+      <Main />
     </>
   );
 };
--- brand-page/src/components/Navigation.jsx
+++ brand-page/src/components/Navigation.jsx
@@ -1,17 +1,17 @@
 const Navigation = () => {
   return (
-    <nav>
+    <nav className="container">
       <div className="logo">
-        <img src="images/brand_logo.png"></img>
+        <img src="/images/brand_logo.png" alt="logo" />
       </div>
       <ul>
-        <li href="#">MENU</li>
-        <li href="#">LOCATION</li>
-        <li href="#">ABOUT</li>
-        <li href="#">CONTACT</li>
+        <li href="#">Menu</li>
+        <li href="#">Location</li>
+        <li href="#">About</li>
+        <li href="#">Contact</li>
       </ul>
 
-      <button>Login</button>
+      <button>login</button>
     </nav>
   );
 };
