f07beefb8ff29ad7382805a84e665d8862aa10e1
pankajsharma81 <pankajsharma88279@gmail.com>
2025-02-28 11:35:53 +0530

    add Navigation

---
+++ brand-page/src/components/Navigation.jsx
@@ -0,0 +1,19 @@
+const Navigation = () => {
+  return (
+    <nav>
+      <div className="logo">
+        <img src="images/brand_logo.png"></img>
+      </div>
+      <ul>
+        <li href="#">MENU</li>
+        <li href="#">LOCATION</li>
+        <li href="#">ABOUT</li>
+        <li href="#">CONTACT</li>
+      </ul>
+
+      <button>Login</button>
+    </nav>
+  );
+};
+
+export default Navigation;
