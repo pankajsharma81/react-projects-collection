24a383b6833d83e983fb6f068dc37ae7fbbf6427
pankajsharma81 <pankajsharma88279@gmail.com>
2025-03-01 09:46:47 +0530

    added Contact Header

--- Contact Us/src/App.jsx
+++ Contact Us/src/App.jsx
@@ -1,10 +1,12 @@
 import "./App.css";
+import ContactHeader from "./components/ContactHeader/contactHeader";
 import Navigation from "./components/Navigation/Navigation";
 
 function App() {
   return (
     <>
       <Navigation />
+      <ContactHeader />
     </>
   );
 }
