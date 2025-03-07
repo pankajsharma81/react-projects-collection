6d936aef4f2ce293402954520142d3de1b180905
pankajsharma81 <pankajsharma88279@gmail.com>
2025-03-07 18:09:32 +0530

    Weather Application

---
+++ Weather Application/src/App.jsx
@@ -0,0 +1,12 @@
+import Button from "@mui/material/Button";
+function App() {
+  return (
+    <>
+      <Button variant="contained" color="success">
+        Success
+      </Button>
+    </>
+  );
+}
+
+export default App;
