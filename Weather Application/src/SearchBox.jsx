9ab62e0c0743ba6bfb59ed8ac1cfa1c8de96916a
pankajsharma81 <pankajsharma88279@gmail.com>
2025-03-08 10:08:50 +0530

    add weather api

--- Weather Application/src/SearchBox.jsx
+++ Weather Application/src/SearchBox.jsx
@@ -6,6 +6,27 @@ import { useState } from "react";
 const SearchBox = () => {
   let [city, setCity] = useState("");
 
+  const API_URL = "https://api.openweathermap.org/data/2.5/weather";
+  const API_KEY = "ce336f1077bec28ec9f32d10b27d94ef";
+
+  let weatherInfo = async () => {
+    let response = await fetch(
+      `${API_URL}?q=${city}&appid=${API_KEY}&units=metric`
+    );
+    let jsonResponse = await response.json();
+    console.log(jsonResponse);
+
+    let result = {
+      temp: jsonResponse.main.temp,
+      tempMin: jsonResponse.main.temp_min,
+      tempMax: jsonResponse.main.temp_max,
+      humidity: jsonResponse.main.humidity,
+      feelsLike: jsonResponse.main.feels_like,
+      weather: jsonResponse.weather[0].description,
+    };
+    console.log(result)
+  };
+
   let handleChange = (event) => {
     setCity(event.target.value);
   };
@@ -14,6 +35,7 @@ const SearchBox = () => {
     event.preventDefault();
     console.log(city);
     setCity("");
+    weatherInfo();
   };
 
   return (
