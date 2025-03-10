da3b02dd8973f2ad097ef3540dd56b06dedc1d45
pankajsharma81 <pankajsharma88279@gmail.com>
2025-03-10 18:25:04 +0530

    Added Image , Icons change feature

--- Weather Application/src/InfoBox.jsx
+++ Weather Application/src/InfoBox.jsx
@@ -4,10 +4,22 @@ import CardMedia from "@mui/material/CardMedia";
 import Typography from "@mui/material/Typography";
 import CardActionArea from "@mui/material/CardActionArea";
 import "./InfoBox.css";
+import AcUnitIcon from '@mui/icons-material/AcUnit';
+import WbSunnyIcon from '@mui/icons-material/WbSunny';
+import ThunderstormIcon from '@mui/icons-material/Thunderstorm';
 
 const InfoBox = ({ info }) => {
   const image_url =
     "https://plus.unsplash.com/premium_photo-1669823619740-20d35e206d66?w=700&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTN8fGNsb3VkeXxlbnwwfHwwfHx8MA%3D%3D";
+
+  const HOT_URL =
+    "https://images.unsplash.com/photo-1524594081293-190a2fe0baae?w=700&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Nnx8aG90JTIwd2VhdGhlcnxlbnwwfHwwfHx8MA%3D%3D";
+
+  const COLD_URL =
+    "https://images.unsplash.com/photo-1612208695882-02f2322b7fee?w=700&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8Y29sZCUyMHdlYXRoZXJ8ZW58MHx8MHx8fDA%3D";
+
+  const RAIN_URL =
+    "https://images.unsplash.com/photo-1613739118925-cde1e8f5d65b?w=700&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8OHx8cmFpbiUyMHdlYXRoZXJ8ZW58MHx8MHx8fDA%3D";
 
   return (
     <div className="InfoBox">
@@ -17,12 +29,24 @@ const InfoBox = ({ info }) => {
             <CardMedia
               component="img"
               height="140"
-              image={image_url}
+              image={
+                info.humidity > 80
+                  ? RAIN_URL
+                  : info.temp > 15
+                  ? HOT_URL
+                  : COLD_URL
+              }
               alt="green iguana"
             />
             <CardContent>
               <Typography gutterBottom variant="h5" component="div">
-                {info.city}
+                {info.city} {
+                  info.humidity > 80
+                    ? <ThunderstormIcon/>
+                    : info.temp > 15
+                    ? <WbSunnyIcon/>
+                    : <AcUnitIcon/>
+                }
               </Typography>
               <Typography
                 variant="body2"
