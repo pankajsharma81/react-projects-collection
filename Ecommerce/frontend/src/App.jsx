a50be0a56df5a6da77e478fd4a4dd77ddc033265
pankajsharma81 <pankajsharma88279@gmail.com>
2025-05-16 23:33:46 +0530

    added react-router-dom

--- Ecommerce/frontend/src/App.jsx
+++ Ecommerce/frontend/src/App.jsx
@@ -1,10 +1,16 @@
 import Navbar from "./Components/Navbar/Navbar";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 
 function App() {
   return (
-    <>
+    <div>
+      <BrowserRouter>
       <Navbar />
-    </>
+      <Routes>
+        <Route />
+      </Routes>
+      </BrowserRouter>
+    </div>
   );
 }
 
