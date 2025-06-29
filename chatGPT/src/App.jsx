c529ac15e23e12a51ff225d42cd3b111fc212043
pankajsharma81 <pankajsharma88279@gmail.com>
2025-06-29 16:52:07 +0530

    basic design

--- chatGPT/src/App.jsx
+++ chatGPT/src/App.jsx
@@ -1,9 +1,24 @@
-import React from 'react'
-
 const App = () => {
   return (
-    <div>App</div>
-  )
-}
+    <div className="grid grid-cols-5 h-screen text-center">
+      <div className="col-span-1 bg-zinc-800">
 
-export default App
+      </div>
+
+      <div className="col-span-4 p-10">
+
+        <div className="container h-110">
+
+        </div>
+
+        <div className="bg-zinc-800 w-1/2 p-1 pr-5 text-white m-auto rounded-4xl border border-zinc-700 flex h-16">
+          <input type="text" className="w-full h-full p-3 outline-none" placeholder="Ask me anything" />
+          <button>Ask</button>
+        </div>
+
+      </div>
+    </div>
+  );
+};
+
+export default App;
